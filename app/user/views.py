fa5c74184f748a20d186a33c08a08289e9a21523
developingmarauder <y.akanza@yahoo.com>
2023-09-24 18:21:27 -0400

    user views and API

---
+++ app/user/views.py
@@ -0,0 +1,10 @@
+"""
+View for the user API
+"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+  serializer_class = UserSerializer
